7efbe0c8067a6f5741b847fa9a9a38d52b7fd900
AlexandraOli <alexandraoliveira.fer@gmail.com>
2020-12-17 21:00:01 -0300

    iniciando js/ janelas dinamicas

--- pag_inicial/script.js
+++ pag_inicial/script.js
@@ -1,9 +1,2 @@
-document.querySelector("#itens")
-.addEventListener("wheel", Event => {
-    if(Event.deltay > 0){
-        Event.target.scrollby(300,0)}
 
-    else {
-        Event.target.scrollby(-300,0)
-    }
- })
+
